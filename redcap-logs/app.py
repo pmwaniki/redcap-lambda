7b7f651bc6b559771f01111cbcdbc1d51cbe74c9
Paul Mwaniki <pmmwaniki06@gmail.com>
2022-05-11 06:24:41 +0300

    Now supports monitoring several servers

--- redcap-logs/app.py
+++ redcap-logs/app.py
@@ -1,14 +1,22 @@
 import datetime
+import decimal
 import json
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o,decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder,self).default(o)
+
 dynamodb=boto3.resource('dynamodb')
 def lambda_handler(event, context):
+    print(">>>>",event)
     table=dynamodb.Table('logs')
-    server=event.get('monitor',None)
-    start=event.get('start',None)
-    stop=event.get('stop',None)
+    server=event['queryStringParameters'].get('monitor',None)
+    start=event['queryStringParameters'].get('start',None)
+    stop=event['queryStringParameters'].get('stop',None)
     if (server is None) or (start is None):
         return {
             'statusCode':400,
@@ -19,13 +27,14 @@ def lambda_handler(event, context):
     if stop is None:
         stop_timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     else:
-        stop_timestamp=stop + " 00:00:00"
-    query=None
+        stop_timestamp=stop + " 23:59:59"
+    response=table.query(KeyConditionExpression=Key('Server').eq(server) & Key('TimeStamp').between(start_timestamp,stop_timestamp),
+                )
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello worlds1",
+            "logs": response['Items'],
             # "location": ip.text.replace("\n", "")
-        }),
+        },cls=DecimalEncoder),
     }
--- redcap-monitor/app.py
+++ redcap-monitor/app.py
@@ -1,3 +1,4 @@
+import grequests
 import logging
 import datetime
 import boto3
@@ -11,6 +12,8 @@ client=boto3.resource("dynamodb")
 timeout=3
 servers=[
     {'servername':'datahubweb','url':'https://data.datahubweb.com/redcap/'},
+    {'servername':'hsu-redcap','url':"https://hsu.kemri-wellcome.org/redcap/"},
+    {'servername':'search-redcap','url':"https://searchtrial.kemri-wellcome.org/"},
 ]
 
 def redcap_website(url,timeout=5):
@@ -24,22 +27,34 @@ def redcap_website(url,timeout=5):
     else:
         return {'code':status_code,'message':req.text}
 
+def get_websites(servers,timeout=5):
+    req=[grequests.get(site['url'],timeout=timeout) for site in servers ]
+    results=grequests.map(req)
+    results2=[]
+    for i in range(len(results)):
+        status_code=results[i].status_code
+        if status_code == 200:
+            results2.append({'server':servers[i]['servername'],'code': 200, 'message': 'OK'})
+        else:
+            results2.append({'server':servers[i]['servername'],'code': status_code, 'message': results[i].text})
+    return results2
+
+
 def lambda_handler(event, context):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    fetch=redcap_website(url='https://data.datahubweb.com',timeout=timeout)
+    results=get_websites(servers=servers,timeout=timeout)
     table = client.Table('logs')
-
-
-    status_code=fetch['code']
-    message=fetch['message']
-    server="datahubweb"
-    table.put_item(
-        Item={
-            'Server':server,
-            'TimeStamp':timestamp,
-            'StatusCode':status_code,
-            'Message':message
-        }
-    )
+    for result in results:
+        status_code=result['code']
+        message=result['message']
+        server=result["server"]
+        table.put_item(
+            Item={
+                'Server':server,
+                'TimeStamp':timestamp,
+                'StatusCode':status_code,
+                'Message':message
+            }
+        )
     print(">>>>logging something")
     logger.info("Full event: %s", event)
